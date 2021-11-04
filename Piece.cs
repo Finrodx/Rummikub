59c98d75bc184df709003ccd938c07f64e8a0de4
salihkaragoz@ogr365.iyte.edu.tr <salihkaragoz@ogr365.iyte.edu.tr>
2021-11-04 19:08:28 +0300

    Add project files.

---
+++ Piece.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rummikub
+{
+    class Piece
+    {
+        public int number { get; set; }
+        private char color  { get; set; }
+
+    public Piece(int number, char color)
+        {
+            this.number = number;
+            this.color = color;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Rummikub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Piece piece = new Piece(1, 'c');
+
+            Console.WriteLine(piece.number);
+        }
+    }
+}
